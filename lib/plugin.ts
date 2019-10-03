7841d6b3bcb2f0e7875c46526d7e6de5217e8ba9
Marios Vertopoulos <mvertopoulos@gmail.com>
2019-10-03 14:03:16 +0300

    Limited properties available to the consumer through the $msal object

--- lib/plugin.ts
+++ lib/plugin.ts
@@ -4,10 +4,18 @@ import { MSAL } from './src/main';
 import { mixin } from "./mixin";
 export const msalMixin = mixin;
 
-
 export default {
     install: (Vue: any, options: Options) => {
-        Vue.prototype.$msal = new MSAL(options);
+        const msal = new MSAL(options);
+        Vue.prototype.$msal = {
+            data: msal.data,
+            signIn() { msal.signIn(); },
+            async signOut() { await msal.signOut(); },
+            isAuthenticated() { return msal.isAuthenticated(); },
+            async acquireToken(request) { return await msal.acquireToken(request); },
+            async callMSGraph() { await msal.callMSGraph(); },
+            saveCustomData(key: string, data: any) { msal.saveCustomData(key, data); }
+        };
         if (options.framework && options.framework.globalMixin) {
             Vue.mixin(mixin);
         }
